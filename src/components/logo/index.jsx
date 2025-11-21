6431f1a1f875789450b93bba05551f59b105de93
Your Name <vnfm0580@gmail.com>
2025-11-21 20:20:44 +0900

    plz

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,6 +1,10 @@
+'use client';
+
 import PropTypes from 'prop-types';
+import { useMemo } from 'react';
 // next
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 // material-ui
 import ButtonBase from '@mui/material/ButtonBase';
@@ -9,10 +13,17 @@ import ButtonBase from '@mui/material/ButtonBase';
 import Logo from './LogoMain';
 import LogoIcon from './LogoIcon';
 import { APP_DEFAULT_PATH } from 'config';
+import { getLocaleBasePath } from '@/utils/getLocaleBasePath';
 
 export default function LogoSection({ reverse, isIcon, sx, to }) {
+  const pathname = usePathname();
+  const localeBasePath = getLocaleBasePath(pathname);
+
+  const defaultHref = useMemo(() => `${localeBasePath}/dashboard`, [localeBasePath]);
+  const resolvedHref = to ?? defaultHref ?? APP_DEFAULT_PATH;
+
   return (
-    <ButtonBase disableRipple component={Link} href={!to ? APP_DEFAULT_PATH : to} sx={sx}>
+    <ButtonBase disableRipple component={Link} href={resolvedHref} sx={sx}>
       {isIcon ? <LogoIcon /> : <Logo reverse={reverse} />}
     </ButtonBase>
   );
